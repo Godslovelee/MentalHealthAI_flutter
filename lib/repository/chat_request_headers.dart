1fbce0d100d9269234be8b5becf4a670fce657b9
Godslove Lee <godslovelee@yahoo.com>
2023-07-10 15:13:03 +0200

    Add ChatRequest Class

---
+++ lib/repository/chat_request_headers.dart
@@ -0,0 +1,6 @@
+import 'package:openai_chat_gpt_flutter/openai/api_key.dart';
+
+ final Map<String, String> headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer $openAiApiKey',
+};
